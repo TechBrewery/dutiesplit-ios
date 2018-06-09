4a7785dfd5de35f49fa5e22965262fcee53da8bc
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-06-09 10:12:00 +0200

    Prepared bindings for manage

--- DutieSplit/Source Files/Modules/Home/Manage/Root/ManageViewModel.swift
+++ DutieSplit/Source Files/Modules/Home/Manage/Root/ManageViewModel.swift
@@ -35,11 +35,47 @@ internal final class ManageViewModel: ViewModel, BindingsSetupable {
         self.dependencies = dependencies
     }
     
+    /// Indicates when duties button was tapped
+    let dutiesButtonTap = PublishSubject<Void>()
+    
+    /// Indicates when settings button was tapped
+    let settingsButtonTap = PublishSubject<Void>()
+    
+    /// Indicates when profile button was tapped
+    let profileButtonTap = PublishSubject<Void>()
+    
+    /// Indicates when switch group button was tapped
+    let switchGroupButtonTap = PublishSubject<Void>()
+    
     /// Indicates when logout button was tapped
     let logoutButtonTap = PublishSubject<Void>()
     
     /// - SeeAlso: BindingsSetupable
     func setupBindings() {
+        dutiesButtonTap
+            .subscribe(onNext: { [unowned self] _ in
+                self.eventTriggered?(.didTapDuties)
+            })
+            .disposed(by: disposeBag)
+        
+        settingsButtonTap
+            .subscribe(onNext: { [unowned self] _ in
+                self.eventTriggered?(.didTapSettings)
+            })
+            .disposed(by: disposeBag)
+        
+        profileButtonTap
+            .subscribe(onNext: { [unowned self] _ in
+                self.eventTriggered?(.didTapProfile)
+            })
+            .disposed(by: disposeBag)
+        
+        switchGroupButtonTap
+            .subscribe(onNext: { [unowned self] _ in
+                self.eventTriggered?(.didTapSwitchGroup)
+            })
+            .disposed(by: disposeBag)
+        
         logoutButtonTap
             .subscribe(onNext: { [unowned self] _ in
                 self.dependencies.authenticationService.removeToken()
