530a281362c76e42aea6e52222dfcaf6aa7be5f9
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-08-03 21:04:46 +0200

    Refactored network service

--- DutieSplit/Source Files/Common/Services/NetworkService.swift
+++ DutieSplit/Source Files/Common/Services/NetworkService.swift
@@ -15,19 +15,19 @@ internal protocol NetworkService {
     /// - Parameter callback: Callback to call when 401 unauthorized error occurred
     func setUnauthorizedErrorCallback(_ callback: @escaping () -> ())
     
-    /// Performs network request and returns Observable of its reponse
+    /// Performs network request and returns Single of its reponse
     ///
     /// - Parameter request: Request to be performed
     /// - Returns: Observable of type Request.Response
-    func perform<Request>(request: Request) -> Observable<NetworkResponseResult<Request.Response>> where Request: NetworkRequest
+    func perform<Request>(request: Request) -> Single<NetworkResponseResult<Request.Response>> where Request: NetworkRequest
     
-    /// Handles response from the server
+    /// Parses response from the server
     ///
     /// - Parameters:
     ///   - dataTaskResponse: Response received from the URLSessionDataTask
     ///   - request: Request used to retrieve the data
-    ///   - observer: Observer for parsed response
-    func handle<Request>(dataTaskResponse: URLSessionDataTaskResponse, for request: Request, observer: AnyObserver<NetworkResponseResult<Request.Response>>) where Request: NetworkRequest
+    /// - Returns: Single with parsed data
+    func parse<Request>(dataTaskResponse: URLSessionDataTaskResponse, for request: Request) -> Single<NetworkResponseResult<Request.Response>> where Request: NetworkRequest
 }
 
 /// Default network application for performing API requests
@@ -57,13 +57,17 @@ internal class DefaultNetworkService: NetworkService {
     }
     
     /// - SeeAlso: NetworkService.perform(request:)
-    func perform<Request>(request: Request) -> Observable<NetworkResponseResult<Request.Response>> where Request: NetworkRequest {
-        return Observable<NetworkResponseResult<Request.Response>>.create { [unowned self] observer in
-            let urlRequest = URLRequest(request: request, authenticationService: self.authenticationService)
-            let task = self.session.dataTask(with: urlRequest) { [weak self] data, response, error in
+    func perform<Request>(request: Request) -> Single<NetworkResponseResult<Request.Response>> where Request: NetworkRequest {
+        return send(request: URLRequest(request: request, authenticationService: authenticationService))
+            .flatMap { [unowned self] in self.parse(dataTaskResponse: $0, for: request) }
+    }
+
+    private func send(request: URLRequest) -> Single<URLSessionDataTaskResponse> {
+        return .create { [unowned self] single in
+            let task = self.session.dataTask(with: request) { data, response, error in
                 let wrappedResponse = HTTPURLResponseWrapper(response: response)
                 let response = URLSessionDataTaskResponse(data: data, response: wrappedResponse, error: error)
-                self?.handle(dataTaskResponse: response, for: request, observer: observer)
+                single(.success(response))
             }
             task.resume()
             return Disposables.create() {
@@ -72,26 +76,21 @@ internal class DefaultNetworkService: NetworkService {
         }
     }
     
-    /// - SeeAlso: NetworkService.handle(dataTaskResponse:for:observer:)
-    func handle<Request>(dataTaskResponse: URLSessionDataTaskResponse, for request: Request, observer: AnyObserver<NetworkResponseResult<Request.Response>>) where Request: NetworkRequest {
+    /// - SeeAlso: NetworkService.parse(dataTaskResponse:for:)
+    func parse<Request>(dataTaskResponse: URLSessionDataTaskResponse, for request: Request) -> Single<NetworkResponseResult<Request.Response>> where Request: NetworkRequest {
         // If error occurred, resolve failure immediately
         if let error = dataTaskResponse.error {
-            observer.onNext(.failure(.connectionError(error)))
-            observer.onCompleted()
+            return .just(.failure(NetworkError.connectionError(error)))
         }
         
         // If the response is invalid, resolve failure immediately
         guard let response = dataTaskResponse.response else {
-            observer.onNext(.failure(.missingResponse))
-            observer.onCompleted()
-            return
+            return .just(.failure(.missingResponse))
         }
         
         // If data is missing, resolve failure immediately
         guard let data = dataTaskResponse.data else {
-            observer.onNext(.failure(.missingData))
-            observer.onCompleted()
-            return
+            return .just(.failure(.missingData))
         }
         
         // Initialize decoder with proper decoding strategy
@@ -102,21 +101,15 @@ internal class DefaultNetworkService: NetworkService {
             // Call unauthorized callback in case of unauthorized status code
             guard response.statusCode != authorizationErrorStatusCode || request is LoginRequest else {
                 authenticationService.removeToken()
-                observer.onNext(.failure(.unauthorized))
-                observer.onCompleted()
                 onUnauthorizedError()
-                return
+                return .just(.failure(.unauthorized))
             }
             
             // Try to parse status from response, otherwise resolve failure only with status code
             guard let parsedErrorStatusResponse = try? decoder.decode(NetworkStatusResponse.self, from: data) else {
-                observer.onNext(.failure(.unacceptableStatusCode(response.statusCode)))
-                observer.onCompleted()
-                return
+                return .just(.failure(.unacceptableStatusCode(response.statusCode)))
             }
-            observer.onNext(.failure(.errorStatus(parsedErrorStatusResponse.status)))
-            observer.onCompleted()
-            return
+            return .just(.failure(.errorStatus(parsedErrorStatusResponse.status)))
         }
         
         // Parse a response
@@ -124,15 +117,13 @@ internal class DefaultNetworkService: NetworkService {
             let parsedResponse = try decoder.decode(Request.Response.self, from: data)
             
             // Resolve success with a parsed response
-            observer.onNext(.success(parsedResponse))
-            observer.onCompleted()
+            return .just(.success(parsedResponse))
         } catch let error {
             #if ENV_DEVELOPMENT
                 // Prints which value failed decoding - only for development
                 print("Decodable error: \(error)")
             #endif
-            observer.onNext(.failure(.responseParseError(error)))
-            observer.onCompleted()
+            return .just(.failure(.responseParseError(error)))
         }
     }
 }
